735fd35db81793db6124250a7a05bc9e45b02966
David <davidhenriq97@gmail.com>
2025-11-05 15:10:42 -0300

    feat: Initialize project with Standalone architecture, Bootstrap integration, and core file hierarchy structure.

---
+++ src/app/pages/update-task/update-task.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-update-task',
+  imports: [],
+  templateUrl: './update-task.component.html',
+  styleUrl: './update-task.component.scss'
+})
+export class UpdateTaskComponent {
+
+}
